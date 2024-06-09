a1f5301ad3d48cc1fc9d961c0428f2f0718f1ac3
Gokarna Adhikari <agokarna444@gmail.com>
2024-06-09 18:58:34 +0545

    complete the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Router>
-        <div className="w-full min-h-screen dark:bg-neutral-950 dark:text-neutral-300 text-neutral-800 bg-neutral-50 flex flex-col">
+        <div className="w-full min-h-screen dark:bg-neutral-950 dark:text-neutral-300 text-neutral-800 bg-neutral-50 flex flex-col overflow-hidden">
           <Navbar />
           <Routes>
             <Route path="/" element={<HomeContainer />} />
--- src/pages/home_container/hero/Hero.jsx
+++ src/pages/home_container/hero/Hero.jsx
@@ -1,10 +1,80 @@
 import React from 'react'
+import Bus1 from "../../../assets/bus.png"
+import Bus2 from "../../../assets/bus5.png"
+import Bus3 from "../../../assets/bus2.png"
+import Bus4 from "../../../assets/bus3.png"
+import Bus5 from "../../../assets/bus4.png"
+
+import { motion } from "framer-motion";
+
 
 const Hero = () => {
+    const imageVariants = {
+        initial: {
+            x: "100%",
+        },
+        animate: {
+            x: "3%",
+            transition: {
+                duration: 3,
+                ease: "easeInOut",
+            },
+        },
+    };
     return (
-        <div className='w-full h-[calc(100vh-8ch)] lg:px-28 md:px-16 sm:px-7 px-4 mt-[8ch] flex items-center justify-center flex-col'>
-            <div className="flex-1 w-full bg-neutral-500">
-                hello render
+        <div className='w-full h-[calc(100vh-8ch)] lg:ps-28 md:ps-16 sm:ps-7 ps-4 mt-[8ch] flex items-center justify-center flex-col hero relative'>
+            <div className="flex-1 w-full flex items-stretch justify-between gap-12 pb-10">
+                <motion.div
+                    className="w-[35%] h-auto rounded-md flex justify-center flex-col space-y-14"
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, ease: 'linear' }}
+                >
+                    <motion.div
+                        className="space-y-5"
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 1, ease: 'linear', delay: 0.2 }}
+                    >
+                        <motion.h1
+                            className="text-7xl font-bold text-neutral-50 leading-[1.15]"
+                            initial={{ opacity: 0, x: -10 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 1.5, ease: 'linear', delay: 0.4 }}
+                        >
+                            Reserve Your Bus <span className="text-violet-400 tracking-wider">Tickets</span> Now
+                        </motion.h1>
+                        <motion.p
+                            className="text-lg text-neutral-300 line-clamp-3 text-ellipsis"
+                            initial={{ opacity: 0, x: -10 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 2, ease: 'linear', delay: 0.6 }}
+                        >
+                            Find and book your bus tickets with just a few clicks. We offer a wide
+                            range of bus routes and schedules to suit your needs.
+                        </motion.p>
+                    </motion.div>
+                    <motion.button
+                        className="w-fit bg-violet-700 hover:bg-violet-800 text-neutral-50 font-medium py-3 px-6 rounded-md ease-in-out duration-300"
+                        initial={{ opacity: 0, x: -10 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 2.5, ease: 'linear', delay: 0.8 }}
+                    >
+                        Reserve Seat Now
+                    </motion.button>
+                </motion.div>
+
+                <div className="w-[70%] h-full rounded-md bg--500 flex items-end justify-end absolute top-0 -right-48">
+                    {/* <img src={Bus5} alt="bus img" className="w-full aspect-[4/2] object-contain" /> */}
+                    <motion.img
+                        src={Bus2}
+                        alt="bus img"
+                        className="w-full aspect-[4/2] object-contain"
+                        initial="initial"
+                        animate="animate"
+                        variants={imageVariants}
+                    />
+                </div>
             </div>
         </div>
     )
