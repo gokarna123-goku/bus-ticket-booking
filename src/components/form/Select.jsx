387e81f45a21926c93bca77da87be9d12b884f5c
Gokarna Adhikari <agokarna444@gmail.com>
2024-06-16 12:05:42 +0545

    working on the own custom select section

---
+++ src/components/form/Select.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Select = () => {
+    return (
+        <div className='w-full'>
+            This is the select section.
+        </div>
+    )
+}
+
+export default Select
